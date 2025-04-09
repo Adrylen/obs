7fe8e1dcc8546b14a354b593638589ab3e4b89f0
Adrylen <adrien.martinez@isen-lille.fr>
2025-04-10 00:55:28 +0200

    feat: configure site for netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/obs/',
-  },
   compatibilityDate: '2024-11-01',
   devtools: {
     enabled: true,
--- pages/overlay/steam-achievements.vue
+++ pages/overlay/steam-achievements.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import type { AsyncData } from '#app';
 import type { Achievement } from '@/types/achievements';
-import type { SteamGameResponse, SteamPlayerStatsResponse, SteamSchemaForGameInput } from '@/types/steam';
+import type { SteamGameResponse, SteamPlayerStatsResponse } from '@/types/steam';
 import AchievementTotal from '@/components/steam-achievements/AchievementTotal.vue';
 import AchievementElement from '@/components/steam-achievements/AchievementElement.vue';
 
@@ -12,11 +11,16 @@ const debug = ref<boolean>(route.query.debug === "true");
 const otherId = ref(route.query.otherId ?? undefined);
 const otherName = ref<string | undefined>(route.query.otherName as string ?? undefined);
 
+const game = ref<SteamGameResponse | undefined>(undefined);
+const response = ref<SteamPlayerStatsResponse | undefined>(undefined);
+const stats = ref<SteamPlayerStatsResponse | undefined>(undefined);
+const otherStats = ref<SteamPlayerStatsResponse | undefined>(undefined);
+
 const queryParameters = (override: object = {}) => Object
   .entries({
     appId: route.query.appId ?? route.query.appid,
     key: route.query.key,
-    l: route.query.lang ?? route.query.l,
+    lang: route.query.lang ?? route.query.l,
     steamId: route.query.steamId ?? route.query.steamid,
     ...override,
   })
@@ -24,12 +28,12 @@ const queryParameters = (override: object = {}) => Object
   .map(([key, value]) => `${key}=${value}`)
   .join('&');
 
-const loaded = computed(() => !!player?.value && !!stats.value && !!game.value);
+const loaded = computed(() => !!response.value && !!stats.value && !!game.value);
 
 const playerAchievements = computed(() => !loaded.value ? [] : game.value?.game
   .availableGameStats.achievements
   .reduce<Array<Achievement>>((array, achievement) => {
-    const { achieved, apiname, unlocktime } = player.value
+    const { achieved, apiname, unlocktime } = response.value
       ?.playerstats.achievements
       ?.find(({ apiname }) => apiname === achievement.name) ?? {};
 
@@ -50,29 +54,20 @@ const playerAchievements = computed(() => !loaded.value ? [] : game.value?.game
   }, []) ?? []
 );
 
-const { data: player } = await useFetch(
-  `https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v1?${queryParameters()}`,
-  { lazy: true, method: 'GET', },
-) as AsyncData<SteamPlayerStatsResponse | undefined, FetchEvent>;
+$fetch<SteamPlayerStatsResponse | undefined>(`/api/steam/player/achievements?${queryParameters()}`)
+  .then((res) => response.value = res)
+  .catch(()=>{});
+$fetch<SteamPlayerStatsResponse | undefined>(`/api/steam/player/stats?${queryParameters()}`)
+  .then((res) => stats.value = res)
+  .catch(()=>{});
+$fetch<SteamGameResponse | undefined>(`/api/steam/game/schema?${queryParameters()}`)
+  .then((res) => game.value = res)
+  .catch(()=>{});
 
-const { data: stats } = await useFetch(
-  `https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v2?${queryParameters()}`,
-  { lazy: true, method: 'GET', },
-) as AsyncData<SteamPlayerStatsResponse | undefined, FetchEvent>;
-
-const { data: game } = await useFetch(
-  `https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2?${queryParameters()}`,
-  { lazy: true, method: 'GET', },
-) as AsyncData<SteamGameResponse | undefined, FetchEvent>;
-
-let otherPlayer = ref<SteamPlayerStatsResponse | undefined>(undefined);
 if (!!otherId) {
-  const { data } = await useFetch(
-    `https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v1?${queryParameters({ steamId: otherId })}`,
-    { lazy: true, method: 'GET', },
-  ) as AsyncData<SteamPlayerStatsResponse | undefined, FetchEvent>;
-
-  otherPlayer = data;
+  $fetch<SteamPlayerStatsResponse | undefined>(`/api/steam/player/achievements?${queryParameters({ steamId: otherId.value })}`)
+  .then((res) => otherStats.value = res)
+  .catch(()=>{});
 }
 
 onMounted(() => {
@@ -84,10 +79,10 @@ onMounted(() => {
 
 <template>
   <section v-if="loaded" class="achievements">
-    <AchievementTotal :data="player" />
+    <AchievementTotal :data="response" />
     <AchievementTotal
       right
-      :data="otherPlayer"
+      :data="otherStats"
       :name="otherName"
     />
     <AchievementElement
@@ -100,7 +95,7 @@ onMounted(() => {
   <template v-if="debug">
     <details>
       <summary>Player achievements</summary>
-      <pre v-if="player">{{ JSON.stringify(player, null, 2) }}</pre>
+      <pre v-if="response">{{ JSON.stringify(response, null, 2) }}</pre>
     </details>
     <details>
       <summary>Player stats</summary>
@@ -108,7 +103,7 @@ onMounted(() => {
     </details>
     <details v-if="otherId">
       <summary>Other player stats</summary>
-      <pre v-if="otherPlayer">{{ JSON.stringify(otherPlayer, null, 2) }}</pre>
+      <pre v-if="otherStats">{{ JSON.stringify(otherStats, null, 2) }}</pre>
     </details>
     <details>
       <summary>Game data</summary>
